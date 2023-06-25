53400d8b09481aa89486fbe9ef4cd50e3dff00f6
neIsonp <nelsonponte37@gmail.com>
2023-06-25 14:40:17 +0000

    added the endpoit because i forgot ahha

--- chat-web/src/main/java/com/nelson/chatweb/controller/AuthController.java
+++ chat-web/src/main/java/com/nelson/chatweb/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,7 @@ public class AuthController {
     this.passwordEncoder = passwordEncoder;
   }
 
+  @PostMapping("/signup")
   public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws UserException{
     String email = user.getEmail();
     String full_name = user.getFull_name();
