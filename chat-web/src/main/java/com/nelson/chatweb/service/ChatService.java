5efb56f1e25be0b3f808f80c081c5733d80bf38f
neIsonp <nelsonponte37@gmail.com>
2023-06-27 15:03:04 +0000

    created a chat service with some methods

---
+++ chat-web/src/main/java/com/nelson/chatweb/service/ChatService.java
@@ -0,0 +1,26 @@
+package com.nelson.chatweb.service;
+
+import java.util.List;
+
+import com.nelson.chatweb.exception.ChatException;
+import com.nelson.chatweb.exception.UserException;
+import com.nelson.chatweb.model.Chat;
+import com.nelson.chatweb.request.GroupChatRequest;
+
+public interface ChatService {
+
+  public Chat createChat(Integer reqUser, Integer userId2) throws UserException;
+
+  public List<Chat> findAllChatByUserId(Integer userId) throws UserException;
+
+  public Chat createGroup(GroupChatRequest req, Integer reqUserId) throws UserException;
+
+  public Chat addUserToGroup(Integer userId, Integer chatId) throws UserException, ChatException;
+
+  public Chat renameGroup(Integer chatId, String groupName, Integer reqUserId) throws ChatException, UserException;
+
+  public Chat removeFromGroup(Integer chatId, Integer userId, Integer reqUser) throws UserException, ChatException;
+ 
+  public Chat deleteChat(Integer chatId, Integer userId) throws ChatException, UserException;
+
+}
