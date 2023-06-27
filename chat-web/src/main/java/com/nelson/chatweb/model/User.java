d95f579cfaad63a2db14492ca12d846de15c2c71
neIsonp <nelsonponte37@gmail.com>
2023-06-27 14:58:18 +0000

    added hascode and equals

--- chat-web/src/main/java/com/nelson/chatweb/model/User.java
+++ chat-web/src/main/java/com/nelson/chatweb/model/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class User {
@@ -30,8 +31,9 @@ public class User {
         this.password = password;
     }
 
+
     public Integer getId() {
-        return id;
+        return this.id;
     }
 
     public void setId(Integer id) {
@@ -39,7 +41,7 @@ public class User {
     }
 
     public String getFull_name() {
-        return full_name;
+        return this.full_name;
     }
 
     public void setFull_name(String full_name) {
@@ -47,27 +49,58 @@ public class User {
     }
 
     public String getEmail() {
-        return email;
+        return this.email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getprofile_picture() {
-        return profile_picture;
+    public String getProfile_picture() {
+        return this.profile_picture;
     }
 
-    public void setprofile_picture(String profile_picture) {
+    public void setProfile_picture(String profile_picture) {
         this.profile_picture = profile_picture;
     }
 
     public String getPassword() {
-        return password;
+        return this.password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(full_name, user.full_name) 
+            && Objects.equals(email, user.email) && Objects.equals(profile_picture, user.profile_picture) 
+            && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, full_name, email, profile_picture, password);
+    }
+ 
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", full_name='" + getFull_name() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", profile_picture='" + getProfile_picture() + "'" +
+            ", password='" + getPassword() + "'" +
+            "}";
+    }
+
+    
 }
 
