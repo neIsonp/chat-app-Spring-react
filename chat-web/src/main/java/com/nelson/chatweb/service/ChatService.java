e0cddc468aabc2bdeccf56775828dec96ab7d557
neIsonp <nelsonponte37@gmail.com>
2023-06-27 20:06:58 +0000

    added some params

--- chat-web/src/main/java/com/nelson/chatweb/service/ChatService.java
+++ chat-web/src/main/java/com/nelson/chatweb/service/ChatService.java
@@ -20,9 +20,9 @@ public interface ChatService {
 
   public Chat addUserToGroup(Integer userId, Integer chatId, User reqUser)  throws UserException, ChatException;
 
-  public Chat renameGroup(Integer chatId, String groupName, Integer reqUserId) throws ChatException, UserException;
+  public Chat renameGroup(Integer chatId, String groupName, User reqUser) throws ChatException, UserException;
 
-  public Chat removeFromGroup(Integer chatId, Integer userId, Integer reqUser) throws UserException, ChatException;
+  public Chat removeFromGroup(Integer chatId, Integer userId, User reqUser) throws UserException, ChatException;
  
   public Chat deleteChat(Integer chatId, Integer userId) throws ChatException, UserException;
   
