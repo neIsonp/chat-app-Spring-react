86e8fbc24da92c2cc6b18802e09dd2ece3b2ce8f
neIsonp <nelsonponte37@gmail.com>
2023-06-28 15:11:31 +0000

    added some methods

--- chat-web/src/main/java/com/nelson/chatweb/service/MessageService.java
+++ chat-web/src/main/java/com/nelson/chatweb/service/MessageService.java
@@ -1,6 +1,9 @@
 package com.nelson.chatweb.service;
 
+import java.util.List;
+
 import com.nelson.chatweb.exception.ChatException;
+import com.nelson.chatweb.exception.MessageException;
 import com.nelson.chatweb.exception.UserException;
 import com.nelson.chatweb.model.Message;
 import com.nelson.chatweb.request.SendMessageRequest;
@@ -8,5 +11,12 @@ import com.nelson.chatweb.request.SendMessageRequest;
 public interface MessageService {
 
   public Message sendMessage(SendMessageRequest req) throws UserException, ChatException;
+
+  public List<Message> getChatsMessages(Integer chatId) throws ChatException;
+
+  public Message findMessageById(Integer messageId) throws MessageException;
+
+  public void deleteMessage(Integer messageId) throws MessageException;
+
    
 }
