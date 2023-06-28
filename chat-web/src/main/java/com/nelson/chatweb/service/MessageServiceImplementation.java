570b05482942555116165a83351745c429c67840
neIsonp <nelsonponte37@gmail.com>
2023-06-28 15:42:47 +0000

    completed  message service implementation

---
+++ chat-web/src/main/java/com/nelson/chatweb/service/MessageServiceImplementation.java
@@ -0,0 +1,77 @@
+package com.nelson.chatweb.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import com.nelson.chatweb.exception.ChatException;
+import com.nelson.chatweb.exception.MessageException;
+import com.nelson.chatweb.exception.UserException;
+import com.nelson.chatweb.model.Chat;
+import com.nelson.chatweb.model.Message;
+import com.nelson.chatweb.model.User;
+import com.nelson.chatweb.repository.MessageRepository;
+import com.nelson.chatweb.request.SendMessageRequest;
+
+public class MessageServiceImplementation implements MessageService{
+
+  private MessageRepository messageRepository;
+  private UserService userService;
+  private ChatService chatService;
+
+  public MessageServiceImplementation(MessageRepository messageRepository, UserService userService, ChatService chatService){
+    this.messageRepository = messageRepository;
+    this.userService = userService;
+    this.chatService = chatService;
+  }
+
+  @Override
+  public Message sendMessage(SendMessageRequest req) throws UserException, ChatException {
+
+    User user = userService.findUserById(req.getUserId());
+    Chat chat = chatService.findChatById(req.getChatId());
+
+    Message message = new Message();
+    message.setChat(chat);
+    message.setUser(user);
+    message.setContent(req.getContent());
+    message.setTimestamp(LocalDateTime.now());
+
+    return message;
+  }
+
+  @Override
+  public List<Message> getChatsMessages(Integer chatId, User reqUser) throws ChatException, UserException {
+    Chat chat = chatService.findChatById(chatId);
+
+    if(!chat.getUsers().contains(reqUser)){
+      throw new UserException("your are not releted to this chat " + chat.getId());
+    }
+
+    List<Message> messages = messageRepository.findByChatId(chat.getId());
+
+    return messages;
+  }
+
+  @Override
+  public Message findMessageById(Integer messageId) throws MessageException {
+    Optional<Message> opt = messageRepository.findById(messageId);
+
+    if(opt.isPresent()){
+      return opt.get();
+    }
+    throw new MessageException("Message not found with id" + messageId);
+  }
+
+  @Override
+  public void deleteMessage(Integer messageId, User reqUser) throws MessageException, UserException {
+    Message message = findMessageById(messageId);
+
+    if(message.getUser().getId().equals(reqUser.getId())){
+      messageRepository.deleteById(messageId);
+    }
+
+    throw new UserException("you can't delete another user's message " + reqUser.getFull_name());
+  }
+  
+}
