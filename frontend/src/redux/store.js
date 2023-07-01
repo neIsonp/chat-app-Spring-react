e6468ba16222313a2243aa7dfad93cb3b7a18d26
neIsonp <nelsonponte37@gmail.com>
2023-07-01 16:30:20 +0000

    added a chat to our store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import { authReducer } from "./auth/Reducer";
+import { chatReducer } from "./chat/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  chat: chatReducer,
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
