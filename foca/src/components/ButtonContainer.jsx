8ae7c3ebdda43a9170cbac1095c225b5ae2719c3
Nelson Pacheco <nelsonponte37@gmail.com>
2023-10-03 15:53:02 +0000

    Delete foca directory

--- foca/src/components/ButtonContainer.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const ButtonContainer = styled.div`
-  margin: 1rem 0 2rem 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
